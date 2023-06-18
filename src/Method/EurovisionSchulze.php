89eba738cad3fec53dbd07cce68e641ca2050008
electric-gecko <liammcgillivray32@gmail.com>
2023-06-17 19:44:32 -0700

    EurovisionSchulze now has fewer calls to Election::getResult()->pairwise, making it much faster. However, there are now some PHP warnings.

--- src/Method/EurovisionSchulze.php
+++ src/Method/EurovisionSchulze.php
@@ -11,22 +11,34 @@ use EurovisionVoting\Contest;
 class EurovisionSchulze extends Schulze_Core
 {
     public const METHOD_NAME = ['Eurovision Schulze', 'Grand Final', 'Grand Final 1.5-root'];
+    protected array $filteredPairwise;
+    
+    protected function getAllPairwise(Contest $contest)
+    {
+        foreach ($contest->votingCountries as $country) {
+            if($contest->votesbyCountry[$country] > 0) {
+                $this->filteredPairwise[$country] = $contest->getResult(methodOptions: ['%tagFilter' => true, 'withTag' => true, 'tags' => $country])->pairwise;
+            }
+        }
+        echo("Finished getAllPairwise\n");
+    }
 
     protected function schulzeVariant(int $i, int $j, Election $contest): int
     {
+        if(!isset($this->filteredPairwise)){
+            $this->getAllPairwise($contest);
+        }
+        
         echo("Starting EurovisionSchulze::schulzeVariant()\n");
         $nationalVotes = $contest->getVotesManager();
         $nationalMargins = [];
         $iCountry = $contest->getCandidateObjectFromKey($i)->getName();
         $jCountry = $contest->getCandidateObjectFromKey($j)->getName();
-        
+        echo("Going to start the foreach loop.\n");
         foreach ($contest->votingCountries as $country)
         {
-            //echo("\n\$country = ".$country."\n\$contest->populations[".$country."] = ".$contest->populations[$country]."\n");
-            
-            if($contest->votesbyCountry[$country] > 0) {
-                $filteredPairwise = $contest->getResult(methodOptions: ['%tagFilter' => true, 'withTag' => true, 'tags' => $country])->pairwise;
-            }
+            echo("\n\$contest->populations[".$country."] = ".$contest->populations[$country]."\n");
+            $filteredPairwise = $this->filteredPairwise[$country];
             $nationalMargins[$country] = (($filteredPairwise[$iCountry]['win'][$jCountry] - $filteredPairwise[$jCountry]['win'][$iCountry] ) * $contest->populations[$country] )^(1/3);
         }
         echo('Compared '.$iCountry.' to '.$jCountry."\n");
