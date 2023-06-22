12ea15404a6341feb87df15ee2487d3f61c15a3e
LiamM32 <liammcgillivray32@gmail.com>
2023-06-22 14:44:06 -0700

    Fixed error in Init.php. RMS output.

--- src/Init.php
+++ src/Init.php
@@ -3,21 +3,25 @@
 declare(strict_types=1);
 
 namespace EurovisionVoting;
-
-use EurovisionVoting\Method\minor_variants;
+//use EurovisionVoting\Method\minor_variants;
 use CondorcetPHP\Condorcet\Condorcet;
 use EurovisionVoting\Method\EurovisionSchulze;
 use EurovisionVoting\Method\EurovisionSchulze2;
 use EurovisionVoting\Method\EurovisionSchulze3;
+use EurovisionVoting\Method\EurovisionSchulze1b;
+use EurovisionVoting\Method\EurovisionSchulze1c;
+use EurovisionVoting\Method\EurovisionSchulze1d;
+
+require_once __DIR__ . '/Method/minor_variants.php';
 
 class Init {
     public static function registerMethods(): void {
         Condorcet::addMethod(EurovisionSchulze::class);
         Condorcet::addMethod(EurovisionSchulze2::class);
         Condorcet::addMethod(EurovisionSchulze3::class);
-        Condorcet::addMethod(minor_variants\EurovisionSchulze1b::class);
-        Condorcet::addMethod(minor_variants\EurovisionSchulze1c::class);
-        Condorcet::addMethod(minor_variants\EurovisionSchulze1d::class);
+        Condorcet::addMethod(EurovisionSchulze1b::class);
+        Condorcet::addMethod(EurovisionSchulze1c::class);
+        Condorcet::addMethod(EurovisionSchulze1d::class);
     }
 }
 
--- tests/benchmark.php
+++ tests/benchmark.php
@@ -27,24 +27,39 @@ $contest->countVotersByCountry();
 
 echo('The entire contest has '.$contest->countVotes()." votes.\n");
 
+$cycles = $argv[2] ?? 10;
 $timetaken = [];
-for ($x = 0; $x <= 9; $x++) {
+$resultsList = [];
+$resultSets = [];
+for ($x = 0; $x < $cycles; $x++) {
     foreach ($Methods as $method) {
         $start_time = microtime(true);
-        $contest->getResult($method);
+        $result = $contest->getResult($method);
         $timetaken[$method][$x] = (microtime(true)-$start_time);
     }
 }
 
 foreach ($Methods as $method) {
-    echo ('Method: '.$method.' Average time: '.array_average($timetaken[$method]).' Max time: '.max($timetaken[$method])."\n");
+    echo ('Method: '.$method.' Average time: '.array_average($timetaken[$method]));
+    echo (' Max time: '.max($timetaken[$method]).' RMS: '.rms($timetaken[$method])."\n");
 }
+
+//end.
 
 function array_average($array) {
     $array = array_filter($array);
+    //echo('first = '.$array[0].', sum = '.array_sum($array).', count = '.count($array)."\n");
     if(count($array)) {
         return array_sum($array)/count($array);
     }
+}
+
+function rms($array) {
+    $products = [];
+    foreach($array as $key=>$entry) {
+        $products[$key] = $entry**2;
+    }
+    return array_sum($products)/array_sum($array);
 }
 
 function makeRandomVotes($q) {
